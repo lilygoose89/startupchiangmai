6002e7ad5851ab1497d0347994b01736894c1a96
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2019-03-24 17:03:48 +0700

    revise baseurl & default layout
    - remove baseurl
    - break up defaullt layout into includes

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,37 +1,37 @@
 
 var documents = [{
     "id": 0,
-    "url": "http://localhost:4000/startupchiangmai/404.html",
+    "url": "http://localhost:4000/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "http://localhost:4000/startupchiangmai/about",
+    "url": "http://localhost:4000/about",
     "title": "About Startup Chiang Mai",
     "body": "Startup Chiang Mai is an effort to shine a light on the startups and entrepreneurs of Chiang MaiWe know they’re out there, so why don’t we all know about them? Maybe it’s because this scrappy crew has more talent than confidence, but we’re not really satisfied to let them hide, tinkering away at their projects. This is a city full of creative people on the brink of a tech revolution, it’s high time these hardworking guys and gals get some recognition. On this website, you will find a directory of information to help you connect with the people you need to move your project forward. You will also find behind-the-scenes stories and get to know the people shaping the startup story of Chiang Mai. We believe it should be easy to find people and information, and access resources and expertise. Why work alone?: Don’t you want to learn new skills? Meet the right people to move your project forward? Connect with new talent and find investors? If we each go it alone, it’s an uphill battle. By sharing knowledge and references we help each other build pathways to success. We believe a rising tide lifts all, and by connecting talented individuals and building a community, we will be able to take a huge collective step forward in developing the startup ecosystem of Chiang Mai, and of Thailand. What will the future bring?: When we tell people we think this quiet, temple-filled city is the next Silicon Valley of Thailand, the typical response is a laugh or a cocked eyebrow. “Why?” they ask. We ask, “WHY NOT?!” While Chiang Mai is more hyped for lifestyle than hustle, it’s a city that manages to bring together an incredibly diverse set of people and skills. We believe this is an asset. This is a city full of thought-leaders, and a conscious class that wants to do more than create the next CandyCrush. What can a city full of passionate people do with somebody shouting their support from the rooftops? You tell us. Better yet, SHOW US. "
     }, {
     "id": 2,
-    "url": "http://localhost:4000/startupchiangmai/blog/",
+    "url": "http://localhost:4000/blog/",
     "title": "Home",
-    "body": "            All Stories:                         "
+    "body": "      All Stories:             "
     }, {
     "id": 3,
-    "url": "http://localhost:4000/startupchiangmai/categories",
+    "url": "http://localhost:4000/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 4,
-    "url": "http://localhost:4000/startupchiangmai/",
-    "title": "",
-    "body": " Posts          Mar 20, 2019    Co-More, Chiang Mai's Collaborative Spaces       subscribe via RSS "
+    "url": "http://localhost:4000/",
+    "title": "Startup Chiang Mai Home",
+    "body": "      Featured:                     All Stories:                                                                 Co-More, Chiang Mai's Collaborative Spaces              :       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi. . . :                                                                                             20 Mar 2019                                            "
     }, {
     "id": 5,
-    "url": "http://localhost:4000/startupchiangmai/robots.txt",
+    "url": "http://localhost:4000/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 6,
-    "url": "http://localhost:4000/startupchiangmai/chiang-mais-collaborative-spaces/",
+    "url": "http://localhost:4000/chiang-mais-collaborative-spaces/",
     "title": "Co-More, Chiang Mai's Collaborative Spaces",
     "body": "2019/03/20 - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Makerspace Chiang Mai: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Niramis Co-Creating Space: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Tha Pae Co-Kitchen: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Weave Artisan Society: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
     }];
